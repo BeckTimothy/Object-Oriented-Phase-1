a3dc520e345f99f241d3a85388c4b759a10ce266
Timothy Beck <johndaveleet@gmail.com>
2019-10-14 20:01:13 -0600

    created dockblock for classes.php

---
+++ classes/classes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Creating author generation class
+ *
+ * I believe I'm creating this php file to create a class that generates author objects.
+ *
+ * @author Timothy Beck <barricuda1993@yahoo.com>
+ */
+class author {
+	/**
+	 * authorId
+	 */
+}
+?>
