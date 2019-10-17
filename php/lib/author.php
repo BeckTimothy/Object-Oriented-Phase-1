3c49446078aa54bfe050cba28441f52deec33fd9
Timothy Beck <johndaveleet@gmail.com>
2019-10-17 15:41:15 -0600

    changed instance of include to require_once for error handling purposes

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,14 +1,14 @@
 <?php
-	use BeckTimothy\ObjectOrientedPhase1\Author;
-	use Ramsey\Uuid\Uuid;
-	include("../classes/Author.php");
+use BeckTimothy\ObjectOrientedPhase1\Author;
+use Ramsey\Uuid\Uuid;
+require_once("../classes/Author.php");
 
-	$timothy = new Author("f7286cc1-5d88-4a91-844e-588cbb940c67",
-		"f6cac5f10e4d14bf1cf85cfec2a0a24c",
-		"http://i.imgur.com/eBa3VXG.jpg",
-		"barricuda1993@yahoo.com",
-		"4a21312be53b16e88c5f78bcfddd16058f773cbc15ead7985b4dcb967dc85c698fcd080fe5ff93a9399c2003084abc3d8",
-		"Tbeck6"
-	);
-	echo($timothy->getAuthorUsername());
+$timothy = new Author("f7286cc1-5d88-4a91-844e-588cbb940c67",
+	"f6cac5f10e4d14bf1cf85cfec2a0a24c",
+	"http://i.imgur.com/eBa3VXG.jpg",
+	"barricuda1993@yahoo.com",
+	"4a21312be53b16e88c5f78bcfddd16058f773cbc15ead7985b4dcb967dc85c698fcd080fe5ff93a9399c2003084abc3d8",
+	"Tbeck6"
+);
+echo($timothy->getAuthorUsername());
 ?>
